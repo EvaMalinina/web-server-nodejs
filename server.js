28a73efce55169234a3636974108bf589af77003
Tatyana Dotsenko <userdotsenko@gmail.com>
2020-03-17 18:08:18 +0200

    done without last optional

--- server.js
+++ server.js
@@ -16,25 +16,6 @@ fs.writeFileSync('./text.parsed.txt', parsed, 'utf8');
 
 //request json file with logs
 let logs = JSON.parse(fs.readFileSync('./index.json'));
-console.log(logs);
-
-//get json for specified date range
-// let startDate = new Date("2020-17-03");
-// let endDate = new Date("2020-18-03");
+console.log(logs, "request");
 
 
-// let resultData = logs.filter(function (a) {
-
-//     let hitDates = a.ProductHits || {};
-//     // extract all date strings
-//     hitDates = Object.keys(hitDates);
-//     // improvement: use some. this is an improment because .map()
-//     // and .filter() are walking through all elements.
-//     // .some() stops this process if one item is found that returns true in the callback function and returns true for the whole expression
-//     hitDateMatchExists = hitDates.some(function(dateStr) {
-//         let date = new Date(dateStr);
-//         return date >= startDate && date <= endDate
-//     });
-//     return hitDateMatchExists;
-// });
-// console.log(resultData);
