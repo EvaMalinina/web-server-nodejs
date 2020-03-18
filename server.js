e3c31619c28341872b634209e6e6823bc758f023
Tatyana Dotsenko <userdotsenko@gmail.com>
2020-03-18 23:16:59 +0200

    fix json

--- server.js
+++ server.js
@@ -1,40 +1,57 @@
 //implement simple web-server
 const http = require('http');
 const fs = require('fs');
+const PORT = './generated.json';
 
 http.createServer((request, response) => {
-  const { method, url } = request;
-  response.writeHead(200, {'content-type': 'text/html'});
-  const {statusMessage : status } = response;
-  response.end(`My the first one web server works! ${JSON.stringify({status})}`);
-
-  const data = JSON.stringify({ logs: [{ method, url }]});
-  fs.writeFileSync('./generated.json', data,  'utf8');
+    const status = 'OK';
+    const { method, url } = request;
+    const data = { method, url, time: Date.now() };
+    createData(data, response);
+    response.writeHead(200, {'Content-type': 'application/json'});
+    response.end(JSON.stringify({ status }));
 }).listen(8081);
 
-//fs module to create/modify files in file system
-let fileContent = fs.readFileSync('./text.txt', 'utf8');
-let parsed = fileContent.replace(/\d/gi, 'Num');
+let createData = (data, response) => {
+    try {
+        let parsedData = {logs: []};
 
-fs.writeFileSync('./text.parsed.txt', parsed, 'utf8');
+        if (fs.existsSync(PORT)) {
+            const generatedData = fs.readFileSync(PORT);
 
-//request json file with logs
-let logs = JSON.parse(fs.readFileSync('./index.json'));
-console.log(logs, "request");
+            if ( generatedData.length ) {
+                parsedData = JSON.parse(generatedData);
+                if (!parsedData.logs) {
+                  parsedData.logs = [];
+                }
+            }
+        }        
 
-//get logs for specified date range;
+        parsedData.logs.push(data);
+
+        parsedData = JSON.stringify(parsedData, null, 2);
+        fs.writeFileSync(PORT, parsedData,  'utf8');
+
+    } catch (error) {
+        response.writeHead(500, {'Content-type': 'application/json'})
+        response.end(JSON.stringify({error: error.message}));
+    }
+};
+
+// get logs for specified date range
+let logs = JSON.parse(fs.readFileSync('./generated.json'));
 let dateRange = ( startDate, endDate ) => {
   for (key in logs) {
     if (logs.hasOwnProperty(key)) {
       
       for ( let i = 0; i < logs[key].length; i++) {
 
-        let givenDate = new Date(logs[key][i]['date']);
+        let givenDate = new Date(logs[key][i]['time']);
         startDate = new Date( startDate ) ;
         endDate = new Date( endDate );
         
         if ( startDate <= givenDate && givenDate <= endDate ) {
-          console.log(`Log in current diapason: ${logs[key][i]['method']} ${logs[key][i]['date']}`);
+          console.log(`Log in current diapason: ${logs[key][i]['method']} ${logs[key][i]['time']}`);
         }
         
       }
